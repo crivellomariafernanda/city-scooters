caeabfaafebdf9afdafefca98d5e538b6a9b7b1c
crivellomariafernanda <105944345+crivellomariafernanda@users.noreply.github.com>
2024-12-08 15:15:35 -0300

    Add Xunit test project and dependencies

---
+++ city-scooters.Tests/SampleTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+public class SampleTests
+{
+    [Fact]
+    public void PassingTest()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+
+    [Fact]
+    public void PassingTest2()
+    {
+        Assert.NotEqual(5, 2 + 2);
+    }
+}
---
+++ city-scooters.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace city_scooters.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
