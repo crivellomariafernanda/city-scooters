3b703b2aaa9c90a08bc6c21bbebf45f6eb8805dd
crivellomariafernanda <105944345+crivellomariafernanda@users.noreply.github.com>
2024-12-08 14:27:47 -0300

    Add swagger config, sql config and first migration

--- city-scooters/Program.cs
+++ city-scooters/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? throw new InvalidOperationException("Connection string"
+        + "'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure Swagger in the request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API v1");
+    });
+}
+
+// Define minimal API endpoints.
+app.MapGet("/", () => "Hello, World!")
+   .WithName("GetRoot")
+   .WithTags("General");
+
+app.MapGet("/api/greet", (string name) => $"Hello, {name}!")
+   .WithName("GreetUser")
+   .WithTags("Greeting");
 
 app.Run();
